b899430a0e911d6c8151c656c83c37070c11eb99
Takhir Galimzianov <tariel@post.com>
2025-05-30 15:01:58 +0400

    Добавил контроллер для получения курса валют

---
+++ CurrencyRateService/Controllers/CurrencyController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyRateService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CurrencyController : ControllerBase
+    {
+        private readonly ICurrencyRateService _service;
+
+        public CurrencyController(ICurrencyRateService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet("{currency}/{date}")]
+        public async Task<IActionResult> Get(string currency, DateTime date)
+        {
+            var rate = await _service.GetRateAsync(currency.ToUpper(), date);
+            return Ok(new { Currency = currency, Date = date.ToShortDateString(), Rate = rate });
+        }
+    }
+
+}
